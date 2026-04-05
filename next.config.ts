e63166129321f373fe40f3daf0492916f1a0337f
shahrierNafis <102902752+shahrierNafis@users.noreply.github.com>
2026-04-05 15:16:25 +0600

    chore: update next.config.ts with export settings and image optimization

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/billboard",
+  assetPrefix: "/billboard/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
